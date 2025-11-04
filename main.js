a239bb7b7594800b4790169533e0b68c6ed77ba2
Arjun <160125274+arjunmalpani@users.noreply.github.com>
2025-11-04 11:19:45 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // API
-const API_KEY = `AIzaSyB8rO374kl_31GYoIH8ERaecp6biLS8Kp0`;
+const API_KEY `takemydih`;
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-exp:generateContent?key=${API_KEY}`;
 
 const typingForm = document.querySelector(".typing-form");
